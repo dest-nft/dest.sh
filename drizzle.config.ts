a1f67b9b9d97bf7da20e177e6326b506d66e0323
Yurii Kabannik <yurii.kabannik@gmail.com>
2023-11-13 01:49:14 +0200

    :wrench: Add drizzle-kit config

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import { config } from "dotenv";
+config({ path: ".env.local" });
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: "./src/services/database/schema.ts",
+  out: "./drizzle/migrations",
+  driver: "pg",
+  dbCredentials: { connectionString: process.env.POSTGRES_URL },
+} satisfies Config;
