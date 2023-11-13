884192d667fe6155b77f6dd7ee575040f521db0f
Yurii Kabannik <yurii.kabannik@gmail.com>
2023-11-13 02:44:52 +0200

    :recycle: Use absolute imports in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,8 @@ config({ path: ".env.local" });
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: "./src/services/database/schema.ts",
-  out: "./drizzle/migrations",
+  schema: "src/services/database/schema.ts",
+  out: "drizzle/migrations",
   driver: "pg",
   dbCredentials: { connectionString: process.env.POSTGRES_URL },
 } satisfies Config;
