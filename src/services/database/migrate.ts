331aec29d9a12c05ec7a362a70758ccc715ab608
Yurii Kabannik <yurii.kabannik@gmail.com>
2023-11-13 02:39:53 +0200

    :hammer: Add migration scripts

---
+++ src/services/database/migrate.ts
@@ -0,0 +1,22 @@
+import { config } from "dotenv";
+config({ path: ".env.local" });
+import { migrate as migrateVercel } from "drizzle-orm/vercel-postgres/migrator";
+import { migrate as migrateNode } from "drizzle-orm/node-postgres/migrator";
+import { db } from "@/services/database/connection";
+
+async function main() {
+  console.log("Running migrations...");
+
+  if (process.env.NODE_ENV === "production") {
+    await migrateVercel(db, { migrationsFolder: "drizzle/migrations" });
+  } else {
+    await migrateNode(db, { migrationsFolder: "drizzle/migrations" });
+  }
+
+  console.log("Migrations completed successfully!");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
