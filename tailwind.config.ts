a1a3da447d076a75074e600e12d23306813b0fd0
Yurii Kabannik <yurii.kabannik@gmail.com>
2023-11-17 10:00:35 +0200

    :sparkles: Add Accordion shared component (#26)
    * :sparkles: Add Accordion shared component
    
    * :art: Format index page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/shared/components/**/*.tsx", "./src/app/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
@@ -16,6 +12,20 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+
+      colors: {
+        gray: "rgba(98, 98, 98, 0.25)",
+        btnBG: "rgba(255, 255, 255, 0.15)",
+        btnBGHeader: "rgba(255,255,255,0.15)",
+        white: "rgba(255, 255, 255, 1)",
+        black: "rgba(0, 0, 0, 1)",
+        green: "rgba(17, 205, 126, 1)",
+        yellow: "rgba(242, 254, 103, 1)",
+        btnBGHeaderHEX: "rgba(26, 26, 26, 1)",
+        btnBGHeaderHEXItem: "rgba(34, 34, 34, 0.5)",
+        borderColor: "rgba(255, 255, 255, 0.7)",
+        transparent: "transparent",
       },
     },
   },
