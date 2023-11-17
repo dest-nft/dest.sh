5efe77a8862d871682af1569fd7503b7a5c82201
Yurii Kabannik <yura_kabannik@icloud.com>
2023-11-17 07:32:49 +0200

    Revert ":recycle: Extract env variables config (#21)"
    This reverts commit 7e3c5d23b2067d32f26be57a303a7ca465b8db61.

--- src/services/database/connection.ts
+++ src/services/database/connection.ts
@@ -1,4 +1,5 @@
-import "@/config";
+import { config } from "dotenv";
+config({ path: ".env.local" });
 import { sql } from "@vercel/postgres";
 import { drizzle as drizzleVercel } from "drizzle-orm/vercel-postgres";
 import { drizzle as drizzleNode } from "drizzle-orm/node-postgres";
