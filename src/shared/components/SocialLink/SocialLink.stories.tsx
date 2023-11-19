976198fdf618528612e0dc4009fec1044544f859
Yurii Kabannik <yurii.kabannik@gmail.com>
2023-11-19 15:18:49 +0200

    :lipstick: Add SocialLink shared component (#28)

---
+++ src/shared/components/SocialLink/SocialLink.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { SocialLink } from "./SocialLink";
+
+const meta: Meta<typeof SocialLink> = {
+  title: "Design System/Social Link",
+  component: SocialLink,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SocialLink>;
+
+export const Twitter = {
+  name: "Twitter",
+  args: {
+    href: "https://twitter.com/",
+    image: "/icons/twitter.svg",
+    width: 34,
+    height: 34,
+  },
+} satisfies Story;
+
+// TODO: Use Discord
+export const LinkedIn = {
+  name: "LinkedIn",
+  args: {
+    href: "https://linkedin.com/",
+    image: "/icons/linkedin.svg",
+    width: 34,
+    height: 34,
+  },
+} satisfies Story;
