6f30fc9f06dec435986a4d79deb5ed0f74523fc2
Yurii Kabannik <yurii.kabannik@gmail.com>
2023-12-04 01:26:04 +0200

    🧑‍💻 Introduce ActionResult (#73)
    * :technologist: Introduce ActionResult
    
    * :label: Add MaybePromise
    
    * :technologist: Add useServerAction hook

---
+++ src/utils/action-result.ts
@@ -0,0 +1,55 @@
+const IDLE = "IDLE";
+const PENDING = "PENDING";
+const SUCCESS = "SUCCESS";
+const FAIL = "FAIL";
+
+export type IdleResult<I = void> = { tag: typeof IDLE; value: I };
+export type PendingResult = { tag: typeof PENDING };
+export type SuccessResult<S> = { tag: typeof SUCCESS; value: S };
+export type FailResult<E> = { tag: typeof FAIL; error: E };
+
+export type ActionResult<I, S, E> =
+  | IdleResult<I>
+  | PendingResult
+  | SuccessResult<S>
+  | FailResult<E>;
+
+export function idle<const I = never>(value?: I): IdleResult<I> {
+  return { tag: IDLE, value: value as I };
+}
+
+export function isIdle<I, S, E>(
+  result: ActionResult<I, S, E>,
+): result is IdleResult<I> {
+  return result.tag === IDLE;
+}
+
+export function pending(): PendingResult {
+  return { tag: PENDING };
+}
+
+export function isPending<I, S, E>(
+  result: ActionResult<I, S, E>,
+): result is PendingResult {
+  return result.tag === PENDING;
+}
+
+export function success<const S = never>(value?: S): SuccessResult<S> {
+  return { tag: SUCCESS, value: value as S };
+}
+
+export function isSuccess<I, S, E>(
+  result: ActionResult<I, S, E>,
+): result is SuccessResult<S> {
+  return result.tag === SUCCESS;
+}
+
+export function fail<const E = never>(error?: E): FailResult<E> {
+  return { tag: FAIL, error: error as E };
+}
+
+export function isFail<I, S, E>(
+  result: ActionResult<I, S, E>,
+): result is FailResult<E> {
+  return result.tag === FAIL;
+}
