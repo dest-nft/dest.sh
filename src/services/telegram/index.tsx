9b58e6f9092b1911eb486e8c010a6154c3f19627
Yurii Kabannik <yurii.kabannik@gmail.com>
2023-12-04 20:17:33 +0200

    :recycle: Change JSX -> MD for telegram message

--- src/services/telegram/index.tsx
+++ src/services/telegram/index.tsx
@@ -1,30 +1,17 @@
 import { Telegraf } from "telegraf";
-import { renderToString } from "react-dom/server";
 import { Contact } from "@/services/database/schema";
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 export async function notifyContact(contact: Contact) {
-  function Message() {
-    return (
-      <>
-        <b>ID:</b> {contact.id}
-        <br />
-        <b>Name:</b> {contact.name}
-        <br />
-        <b>Email:</b> {contact.email}
-        <br />
-        <b>Message:</b>
-        <br />
-        <br />
-        <code>{contact.message}</code>
-      </>
-    );
-  }
+  const message = `
+*ID:* ${contact.id}
+*Name:* ${contact.name}
+*Email:* ${contact.email}
+*Message:*
 
-  await bot.telegram.sendMessage(
-    process.env.TELEGRAM_CHAT_ID,
-    renderToString(<Message />),
-    { parse_mode: "HTML" },
-  );
+${contact.message}
+  `.trim();
+
+  await bot.telegram.sendMessage(process.env.TELEGRAM_CHAT_ID, message);
 }
