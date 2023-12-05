36268e20701652aa4601e26c788ab88cda25011e
Yurii Kabannik <yurii.kabannik@gmail.com>
2023-12-05 04:44:37 +0200

    :lipstick: Add disabled stated to shared Button component

--- src/shared/components/Button/Button.tsx
+++ src/shared/components/Button/Button.tsx
@@ -1,19 +1,23 @@
-import { ButtonHTMLAttributes, ForwardedRef, forwardRef } from "react";
+import { ButtonHTMLAttributes, forwardRef } from "react";
+import { twJoin } from "tailwind-merge";
 
 interface ButtonProps
   extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "className"> {}
 
-export const Button = forwardRef(function Button(
-  { children, ...otherProps }: ButtonProps,
-  ref: ForwardedRef<HTMLButtonElement>,
-) {
-  return (
-    <button
-      {...otherProps}
-      className="flex h-[42px] w-full cursor-pointer items-center justify-center rounded-xl bg-green px-1 text-sm font-medium text-black outline-none hover:bg-yellow md:h-[52px] lg:text-base"
-      ref={ref}
-    >
-      {children}
-    </button>
-  );
-});
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  function Button({ children, disabled, ...props }, ref) {
+    return (
+      <button
+        {...props}
+        className={twJoin(
+          "flex h-[42px] w-full items-center justify-center rounded-xl bg-green px-1 text-sm font-medium text-black outline-none md:h-[52px] lg:text-base",
+          !disabled && "cursor-pointer hover:bg-yellow",
+        )}
+        disabled={disabled}
+        ref={ref}
+      >
+        {children}
+      </button>
+    );
+  },
+);
