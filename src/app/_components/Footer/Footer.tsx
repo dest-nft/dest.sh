9fc7c408ed7db0b4c83f4587b39853cf0449a95c
Yurii Kabannik <yurii.kabannik@gmail.com>
2023-12-16 13:01:52 +0200

    :lipstick: Extract footer component from main

--- src/app/_components/Footer/Footer.tsx
+++ src/app/_components/Footer/Footer.tsx
@@ -5,30 +5,28 @@ import { DevBrotherLink } from "@/app/_components/Footer/DevBrotherLink";
 
 export function Footer() {
   return (
-    <footer className="py-8 md:py-10">
-      <div className="container mx-auto flex flex-wrap items-center gap-4 px-4 sm:justify-between">
-        <div className="flex flex-col">
-          <div className="mb-3 flex">
-            <DestLink />
-          </div>
-          <p className="text-xs">Copyright DEST © 2023</p>
+    <footer className="container mx-auto my-8 flex flex-wrap items-center gap-4 sm:justify-between md:my-10">
+      <div className="flex flex-col">
+        <div className="mb-3 flex">
+          <DestLink />
         </div>
-        <nav className="flex flex-wrap gap-4">
-          <DevBrotherLink />
-          <div className="flex gap-4">
-            <SocialLink
-              href="https://linkedin.com/"
-              image="/icons/linkedin.svg"
-              alt="LinkedIn"
-            />
-            <SocialLink
-              href="https://twitter.com/"
-              image="/icons/twitter.svg"
-              alt="Twitter"
-            />
-          </div>
-        </nav>
+        <p className="text-xs">Copyright DEST © 2023</p>
       </div>
+      <nav className="flex flex-wrap gap-4">
+        <DevBrotherLink />
+        <div className="flex gap-4">
+          <SocialLink
+            href="https://linkedin.com/"
+            image="/icons/linkedin.svg"
+            alt="LinkedIn"
+          />
+          <SocialLink
+            href="https://twitter.com/"
+            image="/icons/twitter.svg"
+            alt="Twitter"
+          />
+        </div>
+      </nav>
     </footer>
   );
 }
