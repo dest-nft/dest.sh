07b701f012895d24c801e04f0d4dcc51eb3ecdee
corners2wall <akosto4kin@gmail.com>
2024-01-23 17:41:31 +0300

    feat: add postgres package

--- src/services/database/connection.ts
+++ src/services/database/connection.ts
@@ -3,12 +3,9 @@ config({ path: ".env.local" });
 import { sql } from "@vercel/postgres";
 import { drizzle as drizzleVercel } from "drizzle-orm/vercel-postgres";
 import { drizzle as drizzleNode } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
 import * as schema from "./schema";
 
 export const db =
   process.env.NODE_ENV === "production"
     ? drizzleVercel(sql, { schema })
-    : drizzleNode(new Pool({ connectionString: process.env.POSTGRES_URL }), {
-        schema,
-      });
+    : drizzleNode(sql, { schema });
