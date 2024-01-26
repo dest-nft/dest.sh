9fc393dd6308d4068e42ebddd4f16114167dc3d1
corners2wall <akosto4kin@gmail.com>
2024-01-26 19:58:26 +0300

    feat: add drizzlePostgres

--- src/services/database/connection.ts
+++ src/services/database/connection.ts
@@ -2,10 +2,14 @@ import { config } from "dotenv";
 config({ path: ".env.local" });
 import { sql } from "@vercel/postgres";
 import { drizzle as drizzleVercel } from "drizzle-orm/vercel-postgres";
-import { drizzle as drizzleNode } from "drizzle-orm/node-postgres";
+import { drizzle as drizzlePostgres } from "drizzle-orm/postgres-js";
+import drizzleConfig from "../../../drizzle.config";
 import * as schema from "./schema";
+import postgres from "postgres";
 
 export const db =
   process.env.NODE_ENV === "production"
     ? drizzleVercel(sql, { schema })
-    : drizzleNode(sql, { schema });
+    : drizzlePostgres(postgres(drizzleConfig.dbCredentials.connectionString), {
+        schema,
+      });
--- src/services/database/migrate.ts
+++ src/services/database/migrate.ts
@@ -1,16 +1,23 @@
 import { config } from "dotenv";
 config({ path: ".env.local" });
 import { migrate as migrateVercel } from "drizzle-orm/vercel-postgres/migrator";
-import { migrate as migrateNode } from "drizzle-orm/node-postgres/migrator";
+import { migrate as migrateNode } from "drizzle-orm/postgres-js/migrator";
 import { db } from "@/services/database/connection";
+import { VercelPgDatabase } from "drizzle-orm/vercel-postgres";
+import * as schema from "./schema";
+import { PostgresJsDatabase } from "drizzle-orm/postgres-js";
 
 async function main() {
   console.log("Running migrations...");
 
   if (process.env.NODE_ENV === "production") {
-    await migrateVercel(db, { migrationsFolder: "drizzle/migrations" });
+    await migrateVercel(db as VercelPgDatabase<typeof schema>, {
+      migrationsFolder: "drizzle/migrations",
+    });
   } else {
-    await migrateNode(db, { migrationsFolder: "drizzle/migrations" });
+    await migrateNode(db as PostgresJsDatabase<typeof schema>, {
+      migrationsFolder: "drizzle/migrations",
+    });
   }
 
   console.log("Migrations completed successfully!");
